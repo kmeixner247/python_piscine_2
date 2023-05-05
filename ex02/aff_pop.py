1dade30e61fca5a0c0bdc40cca2adf0869ce512c
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-05-05 16:40:31 +0200

    final formatting

--- ex02/aff_pop.py
+++ ex02/aff_pop.py
@@ -4,17 +4,33 @@ from load_csv import load
 from matplotlib.ticker import FuncFormatter
 
 
-def millions_formatter(x, pos):
+def millions_formatter(x: float, pos: int) -> str:
     """millions_formatter(x, pos)
 
-Convert axis tick value to millions and format as string with "M" suffix"""
+Convert axis tick value to millions and format as string with "M" suffix
+
+Args:
+    - x (float): Axis tick value.
+    - pos (int): Tick position.
+
+Returns:
+    - str: The formatted tick value with a "M" suffix.
+"""
     return f"{x/1e6:.0f}M"
 
 
-def thousands_formatter(x, pos):
+def thousands_formatter(x: float, pos: int) -> str:
     """thousands_formatter(x, pos)
-    
-Convert axis tick value to millions and format as string with "M" suffix"""
+
+Convert axis tick value to millions and format as string with "M" suffix
+
+Args:
+    - x (float): Axis tick value.
+    - pos (int): Tick position.
+
+Returns:
+    - str: The formatted tick value with a "k" suffix.
+"""
     return f"{x/1e3:.0f}k"
 
 
@@ -24,27 +40,32 @@ def create_plot(df, countries):
 Creates a plot of the life expectancy projections for the given country
 
 Args:
-    - df (pandas.DataFrame): a pandas dataframe with columns for country, year, and life expectancy
-    - country (str): the name of the country to plot data for
+    - df (pd.DataFrame): A pandas dataframe with columns for country, year,
+                         and life expectancy
+    - country (str): The name of the country to plot data for
 
 Returns:
     - None
-    
+
 Raises:
     - AssertionError: If either of the provided arguments has the wrong type.
-    - KeyError: If the provided country doesn't exist as a key in the dataframe.
+    - KeyError: If the provided country doesn't exist as a key in the
+                dataframe.
     - Exception: If there is an unexpected error while creating the plot
 """
     try:
-        assert type(df) is pd.core.frame.DataFrame, "first argument must be a pandas dataframe"
+        assert type(df) is pd.core.frame.DataFrame, "first argument must be a\
+                                                     pandas dataframe"
         assert type(countries) is list, "second argument must be a list"
         for country in countries:
-            assert type(country) is str, "every element of the second argument must be a string"
+            assert type(country) is str, "every element of the second argument\
+                                          must be a string"
         df.set_index('country', inplace=True)
         fig, ax = plt.subplots()
         for country in countries:
             data = df.loc[country]
-            data = data.apply(lambda x: float(x[:-1]) * 1000000 if 'M' in x else float(x[:-1]) * 1000)
+            data = data.apply(lambda x: float(x[:-1]) * 1000000 if 'M' in x
+                              else float(x[:-1]) * 1000)
             data.plot()
         if data.values.max() > 2000000:
             ax.yaxis.set_major_formatter(FuncFormatter(millions_formatter))
@@ -57,17 +78,30 @@ Raises:
     except AssertionError as msg:
         print("create_plot: AssertionError:", msg)
     except KeyError as key:
-        print("create_plot: KeyError: key", key, "could not be found in the dataframe")
+        print("create_plot: KeyError: key", key, "could not be found\
+                                                  in the dataframe")
     except Exception as msg:
         print("create_plot: Error:", msg)
+
 
 def main():
     """main function of aff_pop.py
 
-loads the given path into a pandas dataframe, creates a plot for the given countries and
-displays the plot"""
-    df = load("population_total.csv")
-    create_plot(df, ["Germany", "Netherlands"])
+The population dataframe is loaded using the `load` function from the
+`load_csv` module. Then, a lineplot for Germany vs Netherlands is created using
+the `create_plot` function. Finally, the plot is displayed using `plot.show()`.
+
+Args:
+    None
+
+Returns:
+    None
+
+Raises:
+    see load.__doc__ and create_plot.__doc__
+"""
+    population = load("population_total.csv")
+    create_plot(population, ["Germany", "Netherlands"])
     plt.show()
 
 
